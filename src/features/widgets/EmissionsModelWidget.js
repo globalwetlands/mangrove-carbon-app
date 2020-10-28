5f05ae6054f9b64e66dbf1f6b5bcdccf19825251
Eric Jinks <ericjinks@gmail.com>
2020-10-29 07:49:52 +1000

    Add widget display text

--- src/features/widgets/EmissionsModelWidget.js
+++ src/features/widgets/EmissionsModelWidget.js
@@ -10,17 +10,33 @@ import Spinner from '../../common/Spinner'
 import EmissionModelChart from './EmissionsModelChart'
 
 const EmissionModelDescription = ({ emissionModelResult = {} }) => {
-  const { historicalTimeDiff, loss_ha } = emissionModelResult
-  const displayNumValue = (val) => (_.isNaN(val) ? '_' : val)
+  const {
+    historicalTimeDiff,
+    loss_ha,
+    deforestationRate,
+    sequestrationRate,
+    emissionsFactor,
+  } = emissionModelResult
+  const displayVal = (val, round = 10) =>
+    _.isNaN(val) ? '_' : _.round(val, round)
+  const toPercent = (val) => val * 100
   return (
     <div className="Widgets--Description">
-      <p>
-        Text goes here describing the <strong>data</strong> we're using to{' '}
-        <strong>calculate</strong> the figure below. Mangrove{' '}
-        {loss_ha < 0 ? 'gain' : 'loss'} of{' '}
-        <strong>{displayNumValue(_.round(Math.abs(loss_ha)))} ha</strong> over{' '}
-        <strong>{displayNumValue(historicalTimeDiff)} years</strong>
-      </p>
+      <div>
+        Mangrove {loss_ha < 0 ? 'gain' : 'loss'} of{' '}
+        <strong>{displayVal(Math.abs(loss_ha), 0)} ha</strong> over{' '}
+        {displayVal(historicalTimeDiff)} years.
+      </div>
+      <div>
+        Deforestation rate of{' '}
+        <strong>{displayVal(toPercent(deforestationRate), 2)}% pa</strong>.
+      </div>
+      <div>
+        Sequestration rate of <strong>{displayVal(sequestrationRate)}</strong>.
+      </div>
+      <div>
+        Emissions factor of <strong>{displayVal(emissionsFactor, 2)}</strong>.
+      </div>
     </div>
   )
 }
@@ -63,13 +79,10 @@ function calculateEmissionData(
   const loss_ha = m2ToHa(loss_m2)
   const deforestationRate = loss_ha / area_ha / historicalTimeDiff
 
-  const emissionsFactor =
-    //  (MgC02 per hectare)
-    (agb_tco2e + bgb_tco2e) / area_ha
+  const emissionsFactor = (agb_tco2e + bgb_tco2e) / area_ha //  (MgC02 per hectare)
   // C02 is 2.67 times heavier than C
 
-  const sequestrationRate = 12
-  //    * varies, no global value,
+  const sequestrationRate = 12 //    * varies, no global value,
 
   // generate emission_model data for range of years
   const years = _.range(forecastYears)
@@ -99,7 +112,7 @@ const Debug = ({ emissionModelResult = {} }) => {
   console.log('⚡️: Debug -> emissionModelResult', emissionModelResult)
 
   return (
-    <pre style={{ overflow: 'scroll', height: 150 }}>
+    <pre style={{ overflow: 'scroll', height: 200 }}>
       <code>
         {Object.entries(emissionModelResult).map(([key, value]) => {
           if (_.isArray(value)) {
@@ -114,9 +127,10 @@ const Debug = ({ emissionModelResult = {} }) => {
             </div>
           )
         })}
-        {`\nemission_model({
+        {`\nemissionsFactor = (agb_tco2e + bgb_tco2e) / area_ha`}
+        {`\n\nemission_model({
           t: year,
-          A1: area_ha, // ha
+          A1: area_ha,
           d: deforestationRate,
           Cmax: emissionsFactor,
           s: sequestrationRate,
