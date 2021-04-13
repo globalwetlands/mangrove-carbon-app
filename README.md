ee1f9a2add4cbc6ba396c36edc7158b44d1383c5
Eric Jinks <ericjinks@gmail.com>
2021-04-13 14:50:20 +1000

    Add GH pages deployment

--- README.md
+++ README.md
@@ -44,4 +44,8 @@ It correctly bundles React in production mode and optimizes the build for the be
 The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
+### Deployment: `npm run deploy`
+
+Build the app and push the built app to the `gh-pages` branch. Will be deployed via GitHub pages to https://globalwetlands.github.io/mangrove-carbon-app/
+
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -9,8 +9,8 @@ import { emission_model } from './emission_model'
 export const loadLocationsData = async ({
   type = 'country', // country, aoi, wdpa
 }) => {
-  const dataUrl = `/geojson/${type}_locations.json`
-  console.log('Loading locations data')
+  const dataUrl = `${process.env.PUBLIC_URL}/geojson/${type}_locations.json`
+  console.log('Loading locations data', dataUrl)
   // await delay(1000)
   const data = await fetch(dataUrl).then((res) => res.json())
   return data
@@ -18,7 +18,7 @@ export const loadLocationsData = async ({
 
 export const loadSingleLocationData = async (locationID) => {
   console.log(`Loading location ${locationID} data`)
-  const dataUrl = `/locations_data/${locationID}.json`
+  const dataUrl = `${process.env.PUBLIC_URL}/locations_data/${locationID}.json`
   const data = fetch(dataUrl).then((res) => res.json())
   // await delay(1000)
   return data
